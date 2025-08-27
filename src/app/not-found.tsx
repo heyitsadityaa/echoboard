e5d3f20e8f0aeba290bcde50911327b0e0b6c574
Aditya Prakash <prakaashaditya@gmail.com>
2025-08-27 18:16:24 +0530

    feat: update environment variables for Google authentication and enhance NotFound page UI

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,9 +1,22 @@
+"use client"
+
+import { Button } from '@/components/ui/button'
+import Link from 'next/link'
+import { redirect } from 'next/navigation'
 import React from 'react'
 
 const NotFound = () => {
     return (
-        <div>
-            Not Found
+        <div className='flex flex-col items-center justify-center h-screen text-2xl font-semibold gap-y-2'>
+            <span className="">
+                Page Not Found!!
+            </span>
+
+            <Button className='bg-lime-400 hover:bg-lime-500' size="lg">
+                <Link href="/">
+                    Go to Home
+                </Link>
+            </Button>
         </div>
     )
 }
